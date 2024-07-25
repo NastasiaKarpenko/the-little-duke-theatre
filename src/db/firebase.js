d6c98cf415266c3307e6a4e5ec29d465ac1291ec
NastasiaKarpenko <nastasia.karpenko@gmail.com>
2024-07-25 12:32:24 +0200

    footer updated

--- src/db/firebase.js
+++ src/db/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, getDocs } from 'firebase/firestore';
 import { getAnalytics } from "firebase/analytics";
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -20,5 +20,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { db, collection, getDocs };
+export { db, collection, getDocs, auth };
